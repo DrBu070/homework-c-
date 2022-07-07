f97dfeac854f282712a485f9939bd39e876dbddd
IvanovVladislav <ivanov2703@mail.ru>
2022-07-08 02:58:25 +0300

    add Homework1.3

---
+++ Homework1.3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Задача 6");
+Console.WriteLine("..........................................................");
+
+Console.Write("Ведите целое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % 2 == 0)
+{
+    Console.Write("Число " + numberA +" Четное");
+}
+else
+{
+    Console.Write("Число " + numberA +" Нечетное");
+}
