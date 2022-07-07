6523a2f5767d292c8075454e34a09cfac03453c3
IvanovVladislav <ivanov2703@mail.ru>
2022-07-08 01:42:25 +0300

    Homework1.2

---
+++ Homework1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Задача 4");
+Console.WriteLine("..........................................................");
+
+Console.Write("Ведите первое целое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе целое число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите третье целое число ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+
+if (max < numberB);
+{
+    max = numberB;
+}
+if (max < numberC);
+{
+    max = numberC;
+}
+Console.WriteLine("Максимальным из трех чисел(" + numberA +", " + numberB + ", " + numberC + ") являеться число " + max);
