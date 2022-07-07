1607dc33c6237088f60ed4dced88d5e8d7c73cdb
IvanovVladislav <ivanov2703@mail.ru>
2022-07-08 01:24:29 +0300

    Homework1.1

---
+++ Homework1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задача 2");
+Console.WriteLine("..........................................................");
+
+Console.Write("Ведите первое целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе целое число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("Первое число (" + numberA + ") больше второго (" + numberB + ")");
+}
+else
+{
+    Console.WriteLine("Второе число (" + numberB + ") больше первого (" + numberA + ")");
+}
